1f8c5bcf36b2a42c6ad73757ac69bc8321c5f787
rutvik.eywa <shilabai.c@eywa.com>
2024-11-27 15:49:11 +0530

    user login done

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -10,6 +10,15 @@ router.post('/register',[
   body('password').isLength({min:5}).withMessage('Password must be at least 5 characters')
 ],
 userController.registerUser
+
+)
+
+router.post ('/login',[
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({min:5}).withMessage('Password must be 5 charectors')
+],
+
+userController.loginUser
 )
 
 
